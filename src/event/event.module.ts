9d547eb4d8584a966ca621cbbecdfbf67361b318
Rijal-Muhammad-Kamil <rijalmkamil100@gmail.com>
2025-08-25 14:18:22 +0700

    fix: fixing error start:prod

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -4,11 +4,12 @@ import { EventController } from './event.controller';
 import { EventRepository } from './repository/repository';
 import { UserModule } from '../user/user.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { EventOrganizerRepository } from 'src/event-organizer/repository/repository';
 
 @Module({
   imports: [UserModule, PrismaModule],
   controllers: [EventController],
-  providers: [EventService, EventRepository],
+  providers: [EventService, EventRepository, EventOrganizerRepository],
   exports: [EventService, EventRepository],
 })
 export class EventModule {}
